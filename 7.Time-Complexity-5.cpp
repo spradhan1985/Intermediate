eb5fe8b14afc2d7e4d171bb1467078103b4c5966
spradhan1985 <54279288+spradhan1985@users.noreply.github.com>
2022-08-16 23:53:36 +0530

    Create 7.Time-Complexity-5.cpp

---
+++ 7.Time-Complexity-5.cpp
@@ -0,0 +1,38 @@
+What is the time complexity of following code:
+ 
+int a = 0, i = N; 
+while (i > 0) { 
+    a += i; 
+    i /= 2; 
+}
+
+T.C:O(log 2 base N)
+ 
+8. Time-Complexity-6
+  The complexity of Binary search algorithm is 
+  O(log n)
+9. Time-Complexity-8
+If for an algorithm time complexity is given by O(1) then complexityof it is:
+constant
+10. If for an algorithm time complexity is given by O(log2n) then complexity will: 
+logarthimic 
+11. If for an algorithm time complexity is given by O(n) then complexityof it is:
+linear
+12. If for an algorithm time complexity is given by O((3/2)^n) then complexity will:
+exponential
+13.Time Complexity - 3.0
+  What is the Time Complexity of the below Code?
+  void solve()
+{
+  int i=1;
+  while(i<n)
+  {
+   int x=i;
+    while(x--)
+    {
+      //O(1) operation
+    }
+    i++;
+  }
+T.C: O(n^2)
+  
