f36ca015f61459349551e84374a763fe30e191f8
spradhan1985 <54279288+spradhan1985@users.noreply.github.com>
2022-08-17 00:14:34 +0530

    Create 17. NESTED_CMPL2.cpp

---
+++ 17. NESTED_CMPL2.cpp
@@ -0,0 +1,18 @@
+What is the time complexity of the following code :
+int a=0;
+for(i=0;i<N;i++)
+{
+  for(j=N;j>i;j--)
+  {
+    a=a+i+j;
+  }
+}
+T.C:O(N*N)
+18. Time-Complexity-7
+To measure Time complexity of an algorithm Big O notation is used which:
+describes limiting behaviour of the function
+characterises a function based on growth of function
+upper bound on growth rate of the function
+all of the mentioned
+
+ans: all of the mentioned 
