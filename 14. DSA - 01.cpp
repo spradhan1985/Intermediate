ab6e81b3834e211b58828516ef214bc8700db486
spradhan1985 <54279288+spradhan1985@users.noreply.github.com>
2022-08-16 23:56:47 +0530

    Create 14. DSA - 01.cpp

---
+++ 14. DSA - 01.cpp
@@ -0,0 +1,4 @@
+/*
+An algorithm consists of two independent piece of code, having complexities f(n) and g(n) respectively. What would be the complexity of the complete algorithm
+*/
+MAX( f(n), g(n) )
